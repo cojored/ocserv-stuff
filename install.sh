2cb14eb8eebd21654cd96c3dd76f43c72493582b
cojored <66964845+cojored@users.noreply.github.com>
2021-11-18 15:30:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt install ocserv
-sudo ufw enable
 sudo ufw allow 80,443/tcp
+sudo ufw allow 22/tcp
+sudo ufw enable
 sudo apt install certbot
 sudo apt install apache
 sudo wget -q -O /etc/apache2/sites-available/vpn.dbhm.org.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.dbhm.org.conf
@@ -14,7 +15,10 @@ sudo rm /etc/ocserv/ocserv.conf
 sudo wget -q -O /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/ocserv.conf
 sudo printf "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sudo sysctl -p
-sudo ufw allow 22/tcp
 sudo rm /etc/ufw/before.rules
 sudo wget -q -O /etc/ufw/before.rules https://raw.githubusercontent.com/cojored/ocserv-stuff/main/before.rules
+sudo iptables -t nat -L POSTROUTING
+sudo systemctl restart ufw
+sudo ufw allow 443/tcp
+sudo ufw allow 443/udp
 sudo systemctl restart ufw
