74c99311b74d0cd1ca3e7729eafc71512373d9b4
cojored <66964845+cojored@users.noreply.github.com>
2022-01-26 12:22:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,12 @@ sudo apt install ocserv -y
 sudo ufw allow 80,443/tcp
 sudo ufw allow 22/tcp
 sudo ufw enable
-sudo apt install certbot -y
 sudo apt install apache2 -y
-sudo wget -q -O /etc/apache2/sites-available/vpn.heppcat.com.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.heppcat.com.conf
+sudo wget -q -O /etc/apache2/sites-available/vpn.example.com.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.example.com.conf
 sudo mkdir /var/www/ocserv
 sudo chown www-data:www-data /var/www/ocserv -R
-sudo a2ensite vpn.heppcat.com
-sudo systemctl stop apache2
-sudo certbot certonly --standalone --agree-tos --email admin@heppcat.com -d vpn.heppcat.com -w /var/www/ocserv
-sudo systemctl start apache2
+sudo a2ensite vpn.example.com
+sudo systemctl restart apache2
 sudo rm /etc/ocserv/ocserv.conf
 sudo wget -q -O /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/ocserv.conf
 sudo printf "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
