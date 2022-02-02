816afd766f2acc0e033dfd5af33f26d4b35374d1
cojored <66964845+cojored@users.noreply.github.com>
2022-02-02 09:38:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo ufw allow 80,443/tcp
 sudo ufw allow 22/tcp
 sudo ufw enable
 sudo apt install apache2 -y
-sudo wget -q -O /etc/apache2/sites-available/vpn.example.com.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.example.com.conf
+sudo wget -q -O /etc/apache2/sites-available/vpn.heppcat.com.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.heppcat.com.conf
 sudo mkdir /var/www/ocserv
 sudo chown www-data:www-data /var/www/ocserv -R
-sudo a2ensite vpn.example.com
+sudo a2ensite vpn.heppcat.com
 sudo systemctl restart apache2
 sudo rm /etc/ocserv/ocserv.conf
 sudo wget -q -O /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/ocserv.conf
