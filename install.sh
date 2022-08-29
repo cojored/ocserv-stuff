3a66feffdd407278d7064202136534b885dac8ae
cojored <66964845+cojored@users.noreply.github.com>
2022-08-29 09:15:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install apache2 -y
 sudo wget -q -O /etc/apache2/sites-available/vpn.heppcat.com.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/vpn.heppcat.com.conf
 sudo mkdir /var/www/ocserv
 sudo chown www-data:www-data /var/www/ocserv -R
-sudo a2ensite vpn.heppcat.com
+sudo a2ensite virtnet.heppcat.com
 sudo systemctl restart apache2
 sudo rm /etc/ocserv/ocserv.conf
 sudo wget -q -O /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/cojored/ocserv-stuff/main/ocserv.conf
